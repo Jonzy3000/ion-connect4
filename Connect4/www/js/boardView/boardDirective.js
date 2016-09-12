642c31ab8e4eb89458d0e3db2d30b5f0abaf8f63
Jonzy3000 <mj436@bath.ac.uk>
2016-09-12 11:02:05 -0400

    New York Library

--- Connect4/www/js/boardView/boardDirective.js
+++ Connect4/www/js/boardView/boardDirective.js
@@ -1,13 +1,31 @@
 (function () {
     "use strict";
 
-    var directive = function (boardFactory, gameConstants, turnHandler) {
+    var directive = function (boardFactory, gameConstants, turnHandler, boardHelpers) {
         return {
             scope: {
 
             },
             templateUrl: 'js/boardView/boardDirective.html',
             link: function (scope, element, attrs) {
+                //so dom has access to this variable
+                scope.NUMBER_OF_COLUMNS = gameConstants.NUMBER_OF_COLUMNS;
+
+                scope.getNthColumn = boardHelpers.getNthColumn;
+
+                scope.range = function(start, end) {
+                    console.log(start);
+                    console.log(end);
+                    var rangedArray = [];
+                    for (var i = start; i < end; i++) {
+                        rangedArray.push(i);
+                    }
+
+                    return rangedArray;
+                }
+
+                scope.get
+
                 scope.board = boardFactory.getBoard();
 
                 scope.$watch(function () {
